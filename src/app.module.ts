3730ffc8cc60db2259db2c1c4e3a38ad441c7bdd
alfredaffia <alfredaffia478@gmial.com>
2025-05-19 16:12:00 +0100

    db module config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UploadModule } from './upload/upload.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(), // Loads environment variables
-    MongooseModule.forRoot(process.env.DB_URI || 'Flexwidflash:08024575820@cluster0.c6i2qkb.mongodb.net/file-system'), UploadModule, // Connects to MongoDB
+  imports: [UploadModule,
+        ConfigModule.forRoot({
+      isGlobal: true
+    }), 
+    MongooseModule.forRootAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+   uri: configService.get<string>('DB_URI'),
+   
+      })
+      }),
   ],
 })
 export class AppModule {}
